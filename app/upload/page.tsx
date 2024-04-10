3d773addfe3d81416b6cacc97540ac9a0f42f81d
zfleury <zach@fleury.lol>
2024-04-09 22:20:20 -0500

    data parse

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -2,8 +2,7 @@
 import React, { useState } from 'react';
 
 const Upload = () => {
-  const [data, setData] = useState([]);
-
+  const [data, setData] = useState<{ [key: string]: number } | null>(null);
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) return;
@@ -15,7 +14,8 @@ const Upload = () => {
 
       const data = JSON.parse(content);
       const addresses = data.map((item: any) => item.way_point1__address);
-      setData(addresses);
+      const countedAddresses = countAddress(addresses);
+      setData(countedAddresses);
     };
 
     reader.readAsText(file);
@@ -23,9 +23,45 @@ const Upload = () => {
   return (
     <div>
       <input type="file" id="file" accept=".json" onChange={handleFileChange} />
-      {data}
+      {data && (
+        <div>
+          <h2>Address Count</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Address</th>
+                <th>Count</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Object.entries(data).map(([key, value]) => (
+                <tr key={key}>
+                  <td>{key}</td>
+                  <td>{value}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
 
 export default Upload;
+
+const countAddress = (address: string[]) => {
+  const addressCounted = address.reduce<{ [key: string]: number }>(
+    (acc, curr) => {
+      if (curr in acc) {
+        acc[curr]++;
+      } else {
+        acc[curr] = 1;
+      }
+      return acc;
+    },
+    {}
+  );
+
+  return addressCounted;
+};
