08e60b69a0a6346cbb882375e0a8388800200c2a
zfleury <zach@fleury.lol>
2024-04-10 00:31:56 -0500

    navbar swag

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,27 +1,34 @@
+'use client';
 import React from 'react';
+import Link from 'next/link';
+import classnames from 'classnames';
+
 import { LayoutDashboard, Map, Upload, Building2 } from 'lucide-react';
+import { usePathname } from 'next/navigation';
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const navLinks = [
     {
       name: 'Dashboard',
       icon: LayoutDashboard,
-      path: '/',
+      href: '/',
     },
     {
       name: 'Map',
       icon: Map,
-      path: '/map',
+      href: '/map',
     },
     {
       name: 'Upload',
       icon: Upload,
-      path: '/upload',
+      href: '/upload',
     },
     {
       name: 'Businesses',
       icon: Building2,
-      path: '/businesses',
+      href: '/businesses',
     },
   ];
 
@@ -29,9 +36,16 @@ const NavBar = () => {
     <nav className="bg-red-500 text-white w-fit h-screen">
       <ul className="text-center">
         {navLinks.map((link) => (
-          <li key={link.name} className="flex gap-x-2 p-3 hover:bg-red-600">
+          <li
+            key={link.name}
+            className={classnames({
+              'flex gap-x-2 p-3 hover:bg-red-600': true,
+              'border-r-4': link.href === currentPath,
+              'text-stone-300': link.href !== currentPath,
+            })}
+          >
             <link.icon />
-            <a href={link.path}>{link.name}</a>
+            <Link href={link.href}>{link.name}</Link>
           </li>
         ))}
       </ul>
