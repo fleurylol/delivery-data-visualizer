739e69c172843e26e3e48ca6e08b522abe86bc2c
zfleury <zach@fleury.lol>
2024-04-10 00:16:12 -0500

    snazzy upload button and scroll area

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import React, { useState } from 'react';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 const Upload = () => {
   const [data, setData] = useState<{ [key: string]: number } | null>(null);
@@ -22,26 +23,33 @@ const Upload = () => {
   };
   return (
     <div>
-      <input type="file" id="file" accept=".json" onChange={handleFileChange} />
+      <input
+        type="file"
+        id="file"
+        accept=".json"
+        className="file:bg-green-600 file:text-white file:px-4 file:py-2 file:cursor-pointer file:mt-4 file:mb-4 file:ml-4 file:mr-4 file:font-bold file:shadow-lg file:outline-none file:rounded-md file:hover:bg-green-700 file:hover:transition file:duration-300 file:ease-in-out file:transform file:hover:scale-105 file:active:bg-green-700 file:active:scale-100 file:focus:outline-none file:focus:scale-100 file:focus:bg-green-700"
+        onChange={handleFileChange}
+      />
       {data && (
         <div>
-          <h2>Address Count</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Address</th>
-                <th>Count</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.entries(data).map(([key, value]) => (
-                <tr key={key}>
-                  <td>{key}</td>
-                  <td>{value}</td>
+          <ScrollArea className="h-[700px] w-fit rounded-md border p-4">
+            <table>
+              <thead>
+                <tr>
+                  <th>Address</th>
+                  <th>Count</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {Object.entries(data).map(([key, value]) => (
+                  <tr key={key}>
+                    <td className="pr-2">{key}</td>
+                    <td className="text-center">{value}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </ScrollArea>
         </div>
       )}
     </div>
